602e5908a17c74857cc82458cbe066b5710a4bb1
Christo DeLange <rubygems@dldinternet.com>
2014-06-17 00:36:45 -0400

    Include useful parsing methods

--- lib/dldinternet/mixlib/cli.rb
+++ lib/dldinternet/mixlib/cli.rb
@@ -9,6 +9,41 @@ module DLDInternet
 
       def self.included(receiver)
         receiver.extend(::Mixlib::CLI::ClassMethods)
+
+        receiver.class_eval do
+
+          # --------------------------------------------------------------------------------
+          def parseString(v)
+            v
+          end
+
+          # --------------------------------------------------------------------------------
+          def parsePath(v)
+            File.expand_path(parseString(v))
+          end
+
+          # --------------------------------------------------------------------------------
+          def parseList(v,s=',',method='parseString')
+            parts = []
+            a = v.split(%r/#{s}/)
+            a.each{ |t|
+              parts << send(method,t)
+            }
+            parts
+          end
+
+          # --------------------------------------------------------------------------------
+          def parseOptionString(v,s=',',method='parseString')
+            bags = []
+            if v.match(%r'#{s}')
+              bags << parseList(v,s,method)
+            else
+              bags << send(method,v)
+            end
+            bags.flatten
+          end
+
+        end
       end
 
     end
--- lib/dldinternet/mixlib/cli/version.rb
+++ lib/dldinternet/mixlib/cli/version.rb
@@ -1,7 +1,7 @@
 module DLDInternet
   module Mixlib
     module CLI
-      VERSION = "0.0.4"
+      VERSION = "0.0.5"
     end
   end
 end
